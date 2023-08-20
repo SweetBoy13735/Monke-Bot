d29087b1228c5aa24cd27747b6211cd654529b72
Ramone Graham <ramonegraham@gmail.com>
2023-08-20 07:12:36 +0100

    Create main file

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * Represents the core of the bot.
+ * When the main program starts, this script is responsible for handling the bot's main functions.
+ * @author Ramone Graham
+ */
+// IMPORTS
+// Node modules
+require("dotenv").config();
+
+const { Client, GatewayIntentBits, Events } = require("discord.js");
+
+// CODE BODY
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Execute this handler once when the client is ready
+client.once(Events.ClientReady, readyClient => { console.log(`Ready! Logged-in as ${readyClient.user.tag}.`); });
+
+// Log-in to Discord using the bot token
+client.login(process.env.DISCORD_TOKEN);
